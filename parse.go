1ce3174c46257d1ef5c4da2ffdb01edf6a9cfe64
Sharapatov Nikita <neffshar@gmail.com>
2025-04-22 12:48:01 +0300

    add pars for array

--- parse.go
+++ parse.go
@@ -8,6 +8,8 @@ import (
 	"time"
 )
 
+var repoarray = [2]string{"https://api.github.com/repos/prometheus/prometheus/releases/latest","https://api.github.com/repos/grafana/grafana/releases/latest" }
+
 type Response struct {
 	TagName     string    `json:"tag_name"`
 	PublishedAt time.Time `json:"published_at"`
@@ -15,31 +17,34 @@ type Response struct {
 }
 
 func main() {
-	resp, err := http.Get("https://api.github.com/repos/prometheus/prometheus/releases/latest")
-	if err != nil {
-		fmt.Println("Error making request:", err)
-		return
+	for _, repourl := range repoarray {
+
+		resp, err := http.Get(repourl)
+		if err != nil {
+			fmt.Println("Error making request:", err)
+			return
+		}
+		defer resp.Body.Close()
+	
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response body:", err)
+			return
+		}
+	
+		var result Response
+		if err := json.Unmarshal(body, &result); err != nil {
+			fmt.Println("Error unmarshaling JSON:", err)
+			return
+		}
+	
+		fmt.Println("Latest Prometheus release:")
+		fmt.Printf("Version: %s\n", result.TagName)
+		fmt.Printf("Published at: %s\n", result.PublishedAt.Format("2003-07-23"))
+	
+		fmt.Println("\nPretty printed JSON response:")
+		fmt.Println(PrettyPrint(result))
 	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
-	}
-
-	var result Response
-	if err := json.Unmarshal(body, &result); err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return
-	}
-
-	fmt.Println("Latest Prometheus release:")
-	fmt.Printf("Version: %s\n", result.TagName)
-	fmt.Printf("Published at: %s\n", result.PublishedAt.Format("2003-07-23"))
-
-	fmt.Println("\nPretty printed JSON response:")
-	fmt.Println(PrettyPrint(result))
 }
 
 func PrettyPrint(i interface{}) string {
